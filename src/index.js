f4c44f799a593d5cb84183f2ab2e2ac273873d77
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-07-15 20:21:43 +0430

    Create store

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+import App from './App';
+
 import "./App.sass";
 
-import App from './App';
+import { createStore } from "redux";
+
+import allReducers from './reducers';
+
+let store = createStore(
+  allReducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
