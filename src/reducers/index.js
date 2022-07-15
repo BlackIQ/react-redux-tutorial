912992d06949f3b1788c2096daa8e9858e7c2f9e
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-07-16 01:01:46 +0430

    Update reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,6 +4,8 @@ const todoReducer = (state = [], action) => {
     switch (action.type) {
         case "ADD_TODO":
             return state.concat(action.payload);
+        case "DELETE_TODO":
+            return state.filter(data => data !== action.payload);
         default:
             return state;
     }
