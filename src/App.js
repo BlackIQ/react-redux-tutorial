d961ce4b77faac3a21a24b64259e1f9c465e9faa
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-07-15 22:49:34 +0430

    IDK

--- src/App.js
+++ src/App.js
@@ -1,22 +1,69 @@
-import { useSelector, useDispatch } from "react-redux";
+import {useState} from "react";
 
 import {
-  inc,
-  dec,
+    useSelector,
+    useDispatch
+} from "react-redux";
+
+import {
+    Typography,
+    Container,
+    Grid,
+    AppBar,
+    Toolbar,
+    Button,
+} from "@mui/material";
+
+import {
+    addTodo,
 } from "./actions";
 
-function App() {
-  const counter = useSelector(state => state.counter);
-  const dispatch = useDispatch();
+import AddTodoDialog from "./components/add";
 
-  return (
-    <div className="App">
-      <h1>Counter {counter}</h1>
-      <button onClick={() => dispatch(inc(5))}>Increase</button>
-      &nbsp;
-      <button onClick={() => dispatch(dec(5))}>Decrise</button>
-    </div>
-  );
+function App() {
+    const dispatch = useDispatch();
+
+    const [open, setOpen] = useState(false);
+
+    const todos = useSelector(state => state.counter);
+
+    const handleOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    return (
+        <Container>
+            <AppBar
+                variant="outlined"
+            >
+                <Toolbar>
+                    <Container>
+                        <Typography>
+                            TODO Manager
+                        </Typography>
+                    </Container>
+                </Toolbar>
+            </AppBar>
+            <Toolbar />
+            <Typography
+                variant="h4"
+                color="primary"
+                gutterBottom
+            >
+                TOOD manager with React & Redux!
+            </Typography>
+            <Button onClick={handleOpen}>Open</Button>
+
+            <AddTodoDialog
+                open={open}
+                onClose={handleClose}
+            />
+        </Container>
+    );
 }
 
 export default App;
