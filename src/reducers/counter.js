cc22b9ca89c1a76de6a1985d18d61750ef87c1d6
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-07-15 20:21:23 +0430

    Add counter reducer

---
+++ src/reducers/counter.js
@@ -0,0 +1,12 @@
+const counterReducer = (state = 0, action) => {
+    switch (action.type) {
+        case "INC":
+            return state + 1;
+        case "DEC":
+            return state - 1;
+        default:
+          return state;
+    };
+}
+
+export default counterReducer;
