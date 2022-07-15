c8ca52128688a523522bdbdf221e3abd59c8fc58
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-07-15 20:22:54 +0430

    Add provider

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import App from './App';
 import "./App.sass";
 
 import { createStore } from "redux";
+import { Provider } from 'react-redux';
 
 import allReducers from './reducers';
 
@@ -16,5 +17,7 @@ let store = createStore(
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <App />
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
