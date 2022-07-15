ba7e975d07ca04b999d7a91b6c272585d93103b9
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-07-15 22:45:48 +0430

    Add todo reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,30 +1,18 @@
-import { combineReducers } from "redux";
+import {combineReducers} from "redux";
 
-const counterReducer = (state = 0, action) => {
+const todoReducer = (state = [], action) => {
     switch (action.type) {
-        case "INC":
-            return state + action.payload;
-        case "DEC":
-            return state - action.payload;
-        default:
-          return state;
-    };
-}
-
-const loggedReducer = (state = false, action) => {
-    switch (action.type) {
-        case "SIGN_IN":
-            return !state;
-        case "SIGN_OUT":
-            return !state;
+        case "ADD_TODO":
+            return state.push(action.payload);
         default:
             return state;
     }
+    ;
 }
 
+
 export const allReducers = combineReducers({
-    counter: counterReducer,
-    logged: loggedReducer
+    todo: todoReducer,
 });
 
 export default allReducers;
