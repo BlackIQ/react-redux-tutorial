6667fc9b29ec143e4550d3ba3c6fbc7ba7193d57
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-07-15 21:19:27 +0430

    Add selector and etc

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import { useSelector, useDispatch } from "react-redux";
+
+import {
+  inc,
+  dec,
+} from "./actions";
+
 function App() {
+  const counter = useSelector(state => state.counter);
+  const dispatch = useDispatch();
+
   return (
     <div className="App">
-      <h1>React + Redux</h1>
+      <h1>Counter {counter}</h1>
+      <button onClick={() => dispatch(inc(5))}>Increase</button>
+      &nbsp;
+      <button onClick={() => dispatch(dec(5))}>Decrise</button>
     </div>
   );
 }
