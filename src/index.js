0779cab022d83a240834e4697599d6f39ca3bf9a
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-07-16 14:11:24 +0430

    Load and save stuff

--- src/index.js
+++ src/index.js
@@ -8,12 +8,21 @@ import "./App.sass";
 import {createStore} from "redux";
 import {Provider} from 'react-redux';
 
-import allReducers from './reducers';
+import allReducers from './redux/reducers';
+
+import {loadState, saveState} from "./storage/localstore";
+
+const persistedState = loadState();
 
 let store = createStore(
     allReducers,
+    persistedState,
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
+
+store.subscribe(() => saveState({
+    todo: store.getState().todo
+}));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
